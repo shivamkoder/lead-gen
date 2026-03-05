b9e1c6b865375ef83188e91ed536aec66fa9b6f8
shivamkoder <shivamkumar210107@gmail.com>
2026-03-05 12:14:27 +0530

    Delete my_code directory

--- my_code/backend/database/db.py
+++
@@ -1,6 +0,0 @@
-"""Database initialization and extension setup."""
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-db = SQLAlchemy()
-migrate = Migrate()
--- my_code/backend/migrations/__init__.py
+++
@@ -1,5 +0,0 @@
-# migrations/
-# This directory will be populated by Flask-Migrate
-# Run: flask db init
-# Then: flask db migrate -m "Initial migration"
-# Then: flask db upgrade
