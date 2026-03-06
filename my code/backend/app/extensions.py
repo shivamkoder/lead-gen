3448323c7707b42fedb4df1ce274b22caf226ce8
shivamkoder <shivamkumar210107@gmail.com>
2026-03-06 09:37:55 +0530

    Update extensions.py

--- my code/backend/app/extensions.py
+++ my code/backend/app/extensions.py
@@ -21,7 +21,7 @@ socketio = SocketIO(cors_allowed_origins="*")
 # User loader for Flask-Login
 @login_manager.user_loader
 def load_user(user_id):
-    from app.models import User
+    from backend.app.models import User
     return User.query.get(int(user_id))
 
 
@@ -29,3 +29,4 @@ def load_user(user_id):
 login_manager.login_view = 'auth.login'
 login_manager.login_message = 'Please log in to access this page.'
 login_manager.login_message_category = 'info'
+
